3a4521774c22030d36a8f0db3e2a8a1e07954391
tahir76270 <motahir76270>
2025-10-27 10:56:18 +0530

    url set

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,9 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+}))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/ai', router)
